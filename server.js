2b15e0d283dd177848429f815e68fb68d8751343
Austin Kenny <aibistin.cionnaith@gmail.com>
2018-10-06 17:50:09 -0400

    Using express server to run on Heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+/* Express Server App for launching on Heroku */
+const express = require("express");
+//var path = require ('path');
+/* Heroku usually supplies a port # */
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static(__dirname + "/dist/"));
+
+/* Capture all routes */
+app.get(/.*/, function(req, resp) {
+  res.sendfile(__dirname + "/dist/index.html");
+});
+
+app.listen(port);
+
+console.log("Server started!");
